a4aae85c6b7d189093982efdf96cbd7662f55c83
Hugo Landau <hlandau@devever.net>
2022-07-30 17:29:38 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hlandau/modtest2
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hlandau/modtest"
+)
+
+func main() {
+	fmt.Printf("%v\n", modtest.Value())
+}
